39a8595d3af8bd8a275095d19ca2152942cafacc
ThatOneKidPhillips <42979189+ThatOneKidPhillips@users.noreply.github.com>
2018-10-15 17:08:47 -0400

    Update Files 1
    Update Files 1

---
+++ Models/Greeting.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Attacks1.Models
+{
+    public class Greeting
+    {
+        public string Login { get; set; }
+    }
+}

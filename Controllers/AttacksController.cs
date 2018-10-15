f70f913f14ba6977f6d5e9074eb79b04998b51bc
ThatOneKidPhillips <42979189+ThatOneKidPhillips@users.noreply.github.com>
2018-10-15 17:06:22 -0400

    Update Files
    Update Files

---
+++ Controllers/AttacksController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Attacks1.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab1b.Controllers
+{
+    public class AttacksController : Controller
+    {
+        public IActionResult XSSHello(string name)
+        {
+            string greeting = $"Welcome {name} to our application";
+            // string safeGreeting = System.Net.WebUtility.HtmlEncode(greeting);
+            ContentResult result = new ContentResult();
+            result.ContentType = "text/html";
+            result.Content = greeting;
+            return result;
+        }
+
+        public IActionResult SafeHello(string name)
+        {
+            string greeting = $"Welcome {name} to our application";
+            string safeGreeting = System.Net.WebUtility.HtmlEncode(greeting);
+            ContentResult result = new ContentResult();
+            result.ContentType = "text/html";
+            result.Content = safeGreeting;
+            return result;
+        }
+
+        public IActionResult FormHello(Greeting greeting)
+        {
+            ViewBag.Message = $"Welcome {greeting.Login} to our application";
+            return View(greeting);
+        }
+
+        public IActionResult UnsafeFormHello(Greeting greeting)
+        {
+            ViewBag.Message = $"Welcome {greeting.Login} to our application";
+            return View(greeting);
+        }
+    }
+}
